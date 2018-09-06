4f9ee0fb1c57a42a6e32185ebe9c068561610423
Daven Quinn <dev@davenquinn.com>
2018-09-06 16:34:48 -0500

    Updated formula to specify HEAD

--- Formula/compare-documents.rb
+++ Formula/compare-documents.rb
@@ -2,7 +2,7 @@ class CompareDocuments < Formula
   desc "Script that runs the Microsoft Word 'compare' tool on two documents"
   homepage "https://github.com/davenquinn/compare-documents"
 
-  url "https://github.com/davenquinn/compare-documents/archive/master.zip", :using => :curl
+  head "https://github.com/davenquinn/compare-documents/master.git"
 
   def install
     bin.install "bin/compare-documents"
